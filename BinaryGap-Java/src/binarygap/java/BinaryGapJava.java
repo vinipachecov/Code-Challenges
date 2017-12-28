6639652c24bdd3f5f2783e414c5c72ab4b5fbe48
vinicius p vieira <vinipachecov@gmail.com>
2017-12-28 14:32:51 -0200

    binaryGap in Java

--- BinaryGap-Java/src/binarygap/java/BinaryGapJava.java
+++ BinaryGap-Java/src/binarygap/java/BinaryGapJava.java
@@ -14,8 +14,46 @@ public class BinaryGapJava {
     /**
      * @param args the command line arguments
      */
+    
+//    if (convertedNumber[index] === "1") {
+//          if(foundAone){
+//              if(binGapLength === '0'){
+//                binGapLength = auxCont;
+//              } else if(auxCont > 0){                  
+//                    binGapLength =  auxCont > binGapLength ? auxCont : binGapLength;                  
+//              }              
+//          }
+//          foundAone = true;                              
+//          auxCont = 0;
+//      }
+//      if(foundAone === true && convertedNumber[index] === '0'){
+//          auxCont++;
+//      }            
     public static void main(String[] args) {
-        // TODO code application logic here
+        String convertedNumber = (Integer.toBinaryString(5));
+        
+        boolean foundAone = false;
+        int BinGapLength = 0;
+        int auxCount = 0;
+        
+        
+        for (int i = 0; i < convertedNumber.length(); i++) {
+            if(convertedNumber.charAt(i) == '1'){
+                if(foundAone){
+                    if(BinGapLength == 0){
+                        BinGapLength = auxCount;
+                    } else if (auxCount > 0) {
+                        BinGapLength = auxCount > BinGapLength ? auxCount : BinGapLength;
+                    }
+                }
+                foundAone = true;
+                auxCount = 0;
+            }
+            if(foundAone == true && convertedNumber.charAt(i) == '0'){
+                auxCount++;
+            }
+        }
+        System.out.println(BinGapLength);
     }
     
 }
